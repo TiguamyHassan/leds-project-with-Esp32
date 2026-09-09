053669a2a393ddc340d4f683e276b4556617264c
TiguamyHassan <hassantiguamy@gmail.com>
2026-09-09 12:03:45 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace led_turn_off
+{
+    public partial class MainPage : ContentPage
+    {
+        HttpClient client = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void redLed(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+                await client.GetAsync("http://IPonred");
+            else
+                await client.GetAsync("http://IP/offred");
+        }
+
+        async void greenLed(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+                await client.GetAsync("http://IP/ongreen");
+            else
+                await client.GetAsync("http://IP/offgreen");
+        }
+
+        async void blueLed(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+                await client.GetAsync("http://IP/onblue");
+            else
+                await client.GetAsync("http://IP/offblue");
+        }
+    }
+}
